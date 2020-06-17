2410e67493f22465d056aa24451d19472c0f7ad3
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-06-17 16:20:26 -0400

    Adds GET and POST methods to order_item model

--- routes/order_items.js
+++ routes/order_items.js
@@ -1,7 +1,49 @@
 var express = require("express");
 var router = express.Router();
-const { Items, Orders, OrderItems } = require("../database/models");
+const { Items, OrderItems } = require("../database/models");
 
+router.get("/", async (req, res, next) => {
+    // try to get campuses object from database
+    try {
+      // campuses will be the result of the Campus.findAll promise
+      const allOrderItems = await OrderItems.findAll();
+      // if campuses is valid, it will be sent as a json response
+      console.log(allOrderItems);
+      res.status(200).json(allOrderItems);
+    } catch (err) {
+      // if there is an error, it'll passed via the next parameter to the error handler middleware
+      next(err);
+    }
+  });
+
+// Route to handle adding a campus
+// /api/campuses/
+router.post("/", async (req, res, next) => {
+    // Take the form data from the request body    
+    const { quantity, orderId, itemId} = req.body;
+   
+    // Create a campus object
+    try{
+        console.log(itemId);
+        const item = await Items.findByPk(itemId);
+        const iPrice = item.price;
+         const orderItemObj = {
+            quantity: quantity,
+            price: iPrice,           
+            orderId: orderId,
+            itemId: itemId,
+    };
+            // Create a new campus on the database
+        const newOrderItem = await OrderItems.create(orderItemObj);
+        // The database would return a campus
+        // send that campus as a json to the client
+        res.status(201).send(newOrderItem);
+    }
+    catch (err){
+        next(err);
+    }   
+   
+  });
 /*
 router.put("/:id", async (req, res, next) => {
     // get the id from request params
