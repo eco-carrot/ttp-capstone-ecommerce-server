c23788d10d46b3d33536ad4c3fb843babafc4913
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-06-18 11:26:32 -0400

    Updated User Relaitonal Model

--- database/models/users.js
+++ database/models/users.js
@@ -5,7 +5,8 @@ const Users = db.define("users", {
   lastName: { type: Sequelize.STRING, allowNull: false },
   firstName: { type: Sequelize.STRING, allowNull: false },
   email: { type: Sequelize.STRING, allowNull: false, validate: {isEmail: true}, unique:true },    
-  role: { type: Sequelize.STRING, allowNull: false },    
+  role: { type: Sequelize.STRING, allowNull: false }, 
+  password: {type: Sequelize.STRING, allowNull: false},    
   imageUrl: {
     type: Sequelize.STRING,
     defaultValue: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRRWAjTO9sDIgksF8zbkTZ7rjZqqwJmYqKEJhcemmfYkwFn3kkz&usqp=CAU",
