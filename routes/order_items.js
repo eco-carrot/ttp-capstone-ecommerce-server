02a43920ab0450421a9b9927644dd84f070c8cf0
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-06-17 22:28:00 -0400

    CRUD for order_items relational model

--- routes/order_items.js
+++ routes/order_items.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const { Items, OrderItems } = require("../database/models");
+const Orders = require("../database/models/orders");
 
 router.get("/", async (req, res, next) => {
     // try to get OrderItems object from database
@@ -16,13 +17,28 @@ router.get("/", async (req, res, next) => {
     }
   });
 
+router.get("/:id", async (req, res, next) => {
+    // try to get OrderItems for a specific order from database   
+    try {
+      // OrderItems will be the result of the OrderItems.findAll promise
+      const allOrderItems = await OrderItems.findAll({where: 
+        {orderId: req.params.id}});
+      // if OrderItems is valid, it will be sent as a json response
+      
+      res.status(200).json(allOrderItems);
+    } catch (err) {
+      // if there is an error, it'll passed via the next parameter to the error handler middleware
+      next(err);
+    }
+  });
+
 // Route to handle adding a OrderItem
-// /api/orders/
+// /api/order_items/
 router.post("/", async (req, res, next) => {
     // Take the form data from the request body    
     const { quantity, orderId, itemId} = req.body;
    
-    // Create a campus object
+    // Create a OrderItem object
     try{        
         const item = await Items.findByPk(itemId);
         const iPrice = item.price;
@@ -32,10 +48,10 @@ router.post("/", async (req, res, next) => {
             orderId: orderId,
             itemId: itemId,
     };
-            // Create a new campus on the database
+            // Create a new OrderItem on the database
         const newOrderItem = await OrderItems.create(orderItemObj);
-        // The database would return a campus
-        // send that campus as a json to the client
+        // The database would return a OrderItem
+        // send that OrderItem as a json to the client
         res.status(201).send(newOrderItem);
     }
     catch (err){
@@ -45,39 +61,88 @@ router.post("/", async (req, res, next) => {
   });
 
 
-router.put("/", async (req, res, next) => {
-    // get the id from request params
-    //const { orderId, itemId } = req.params;
+router.put("/:id/:itemID", async (req, res, next) => {
+    // get the id from request params    
+    const { id, itemID} = req.params;
     // get form data from the request body
-    const { orderId, itemId, quantity } = req.body;
+    const { quantity } = req.body;
     
-    try {
-      // if successfull:
-      // Find a OrderItem with a matching id from the database
-      console.log(itemId + " " + orderId + " " + quantity);
-      const item = await Items.findByPk(itemId);
-      const iPrice = item.price;
-      console.log(item);
+    //try ro find, update, and send data for a specific OrderItems object
+    try {         
+      //get a current price for the Item associated with this OrderItems object
+      const item = await Items.findByPk(itemID);
+      const iPrice = item.price;      
 
+      //store the data needed for the update in an object
       const updatedObj = {
           quantity: quantity,    
-          price: iPrice  
-        };      
-
-      const orderItem = await OrderItems.findByPk(orderId);
-      // database would return a valid OrderItem object or an error
-      console.log(updatedObj);
-      // modify the OrderItem object with new form data
-      await orderItem.set(updatedObj);
-      // save the new OrderItem object to the data
-      // database would return a new OrderItem object
-      const updatedOrderItem = await orderItem.save();
-      console.log(updatedOrderItem);
+          price: iPrice 
+        }; 
+        
+    //Find and update OrderItem with a matching id (orderId) and itemId from the database
+      const orderItem = await OrderItems.update(
+          {
+            quantity: updatedObj.quantity,
+            price: updatedObj.price 
+          },
+          {
+            where: {orderId: id,
+            itemId: itemID}
+        });
+      
+      //find the updated object, convert it to JSON, and save data as updatedOrderItem
+      const updatedOrderItem = await OrderItems.findAll(
+          {
+            where: {orderId: id,
+                itemId: itemID},
+            raw: true
+          }
+      );     
+     
       // send the new OrderItem as a response from the API
       res.status(201).send(updatedOrderItem);
     } catch (err) {
       // if error:
       // handle the error
+      next(err);
+    }
+  });
+
+// Route to handle removing OrderItem
+// /api/order_items/:id/:itemID
+router.delete("/:id/:itemID", async (req, res, next) => {
+    const { id, itemID } = req.params;
+       
+    // get an id and itemID for a OrderItem to delete
+    try {
+      // pass the id and itemID to the database to find OrderItem 
+      //and delete it
+      await OrderItems.destroy({
+        where: {orderId: id,
+        itemId: itemID}
+        });
+      // send a success message to the client
+      res.sendStatus(204);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+
+  //delete ALL OrderItems associated with the order (id)
+  router.delete("/:id", async (req, res, next) => {    
+    // get an id for a OrderItems to delete
+    const { id } = req.params;
+    try {
+      // pass the id (orderId) and itemId to the database to find OrderItem 
+      //and delete it
+      await OrderItems.destroy({
+        where: {orderId: id,
+        }
+        });
+      // send a success message to the client
+      res.sendStatus(204);
+    } catch (err) {
       next(err);
     }
   });
